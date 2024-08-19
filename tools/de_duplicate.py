671d08608332ced5892cf85d9ef56e1ff7044117
maomao0819 <maomao.08190@gmail.com>
2024-08-19 13:57:43 +0800

    feat: add args for tools

--- tools/de_duplicate.py
+++ tools/de_duplicate.py
@@ -1,10 +1,19 @@
 import json
 from collections import OrderedDict
+import argparse
 
-with open("A.json", "r", encoding="utf-8") as file:
-    data = json.load(file)
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--input_file", type=str,
+                        default="../CoT/cot_cwq.jsonl", help="choose the input file.")
+    args = parser.parse_args()
 
-result = list(OrderedDict((item['question'], item) for item in data).values())
+    with open(args.input_file, "r", encoding="utf-8") as file:
+        data = [json.loads(line) for line in file]
 
-with open("B.json", "w", encoding="utf-8") as file:
-    json.dump(result, file, ensure_ascii=False)
+    result = list(OrderedDict((item['question'], item) for item in data).values())
+
+    with open(args.input_file, "w", encoding="utf-8") as file:
+        for item in result:
+            json.dump(item, file, ensure_ascii=False)
+            file.write("\n")
--- tools/jsonl2json.py
+++ tools/jsonl2json.py
@@ -1,4 +1,6 @@
 import json
+import argparse
+
 
 def jsonl_to_json(jsonl_file, json_file):
     with open(jsonl_file, 'r') as infile:
@@ -7,6 +9,10 @@ def jsonl_to_json(jsonl_file, json_file):
             json_list = [json.loads(line) for line in json_lines]
             json.dump(json_list, outfile, indent=4)
 
-# 用法示例
-jsonl_to_json('../CoT/cot_cwq.jsonl', '../CoT/CoT_cwq.json')
-jsonl_to_json('../ToG/ToG_cwq.jsonl', '../ToG/ToG_cwq.json')
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--input_file", type=str,
+                        default="../CoT/cot_cwq.jsonl", help="choose the input file.")
+    args = parser.parse_args()
+
+    jsonl_to_json(args.input_file, args.input_file[:-1])
